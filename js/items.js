43dc5f47610c7abb6a4cc81ee007b4a42f0c034d
Magnus <magnus.ww@hotmail.com>
2023-10-30 17:24:50 +0100

    added some minor comments

--- js/items.js
+++ js/items.js
@@ -1,4 +1,6 @@
 let jsonobjects;
+
+//creates a div element and adds content to it from the json object passed to it.
 function appendItemToHtml(item) {
     let div = document.createElement('div')
     div.className = 'products-item';
@@ -9,7 +11,7 @@ function appendItemToHtml(item) {
     document.querySelector('.products').appendChild(div)
 
 };
-
+//takes a filter, defined by the selection box, and populates the webpage with the items that match the filter.
 function populateItems(filter) {
     document.querySelector('.products').innerHTML = ''
     for (let i = 0; i < jsonobjects.length; i++) {
@@ -18,12 +20,13 @@ function populateItems(filter) {
         }
     }
 }
-
+//fetches the json data and populates the webpage on first load.
 fetch("../json/items.json").then((response) => response.json())
     .then(function (json) {
         jsonobjects = json
         populateItems('all')
     });
+//repopulates the webpage each time a filter value is selected.
 document.querySelector('#products').addEventListener('click', function (event) {
     console.log(this.value)
     populateItems(this.value)
